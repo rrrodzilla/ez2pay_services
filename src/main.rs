25ddad25b5c2905b8aa8a86d1dfa91e5a0f95f10
rodzilla <rolandrodriguez@gmail.com>
2021-02-28 16:20:39 -0600

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,78 @@
+#[macro_use]
+extern crate log;
+
+//use actix_service::Service;
+use actix_web::dev::Service;
+use actix_web::http::header::CONTENT_TYPE;
+use actix_web::http::HeaderValue;
+use actix_web::{get, post, web, App, HttpMessage, HttpResponse, HttpServer, Responder, Result};
+use clap::Clap;
+//use futures::future::FutureExt;
+use pickledb::{PickleDb, PickleDbDumpPolicy};
+use serde::{Deserialize, Serialize};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[post("/run")]
+async fn run(account: web::Json<Account>) -> Result<String> {
+    info!("executing run");
+    Ok(format!("Welcome {}!", account.phone_number))
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "ez2pay=Info");
+    env_logger::init();
+    info!("Starting server...");
+    HttpServer::new(|| {
+        App::new()
+            .wrap_fn(|req, srv| {
+                info!("Hi from one. You requested: {}", req.path());
+                let fut = srv.call(req);
+                async {
+                    let mut res = fut.await?;
+                    res.headers_mut()
+                        .insert(CONTENT_TYPE, HeaderValue::from_static("text/plain"));
+
+                    info!("Updated response one");
+                    Ok(res)
+                }
+            })
+            .wrap_fn(|req, srv| {
+                info!("Hi from two. You passed: {:#?}", req);
+                let fut = srv.call(req);
+                async {
+                    let mut res = fut.await?;
+                    res.headers_mut()
+                        .insert(CONTENT_TYPE, HeaderValue::from_static("text/plain"));
+
+                    info!("Updated response two");
+                    Ok(res)
+                }
+            })
+            .service(hello)
+            .service(run)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+struct Account {
+    phone_number: String,
+}
