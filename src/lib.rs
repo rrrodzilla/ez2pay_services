a4683991cecbf5802d327b8bbd90818b7e41e43d
rodzilla <rolandrodriguez@gmail.com>
2021-02-28 17:00:31 -0600

    account struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,35 @@
+//our account consists of a phone number, it's always tied to
+//the phone used to contact the service
+pub struct Account {
+    phone_number: String,
+    stripe_id: String,
+    subscriber: bool,
+}
+impl Account {
+    fn new(id: String) -> Self {
+        Account {
+            phone_number: id,
+            stripe_id: String::from(""),
+            subscriber: false,
+        }
+    }
+}
+pub struct Customer {}
+pub struct Order {}
+pub struct Product {}
 pub fn test() {
     println!("Test");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_account() {
+        let p = String::from("+12063832022");
+        let a = Account::new(p.clone());
+        assert_eq!(p, a.phone_number);
+        assert_eq!(false, a.subscriber);
+    }
 }
